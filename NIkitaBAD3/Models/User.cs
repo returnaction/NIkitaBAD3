f9a080f2868a08c4ce4b93e5a8ebbeca79c2eb83
returnaction <obergannv@gmail.com>
2024-03-18 23:03:55 -0400

    Add User Class

---
+++ NIkitaBAD3/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace NIkitaBAD3.Models
+{
+    public class User : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        public string? Casino { get; set; }
+        public string? Position { get; set; }
+
+        public string? StreetAddress { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? Country { get; set; }
+
+    }
+}
